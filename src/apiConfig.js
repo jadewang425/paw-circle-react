9bb60fa2c5bcec26a0faa6f6a254cd3df8cc0991
jadewang425 <jadewang425@gmail.com>
2023-10-13 14:18:44 -0400

    add deployed api url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: '<replace_with_deployed_api_url>',
+	production: 'https://paw-circle-api.fly.dev',
 	development: 'http://localhost:8000',
 }
 
